dad223ad88572939973889be90181ac74c35b7e0
Maxim Piessen <maximpiessen@Maxims-MacBook-Pro.local>
2021-12-13 12:06:47 +0100

    add random receiver

--- app/src/components/Airdrop/index.js
+++ app/src/components/Airdrop/index.js
@@ -1,12 +1,12 @@
 import {Button} from "@material-ui/core";
-import { LAMPORTS_PER_SOL, TransactionSignature } from '@solana/web3.js';
+import { LAMPORTS_PER_SOL, TransactionSignature, PublicKey } from '@solana/web3.js';
 const { TOKEN_PROGRAM_ID, Token, ASSOCIATED_TOKEN_PROGRAM_ID } = require("@solana/spl-token");
 import {useWallet} from '@solana/wallet-adapter-react';
 import {BN, Program, utils, web3} from '@project-serum/anchor';
 const { SystemProgram } = web3;
 import {useNotify} from '../Utils/notify';
 import Select from 'react-select';
-import React, {FC, useState} from 'react';
+import React, {FC, useState, useEffect} from 'react';
 import {GetProvider} from '../Utils/utils';
 import {programID, dummyMintPk, dummyMintPkBump} from '../../config/config.js';
 import idl from '../../config/spl_token_faucet.json';
@@ -19,12 +19,19 @@ const AirDrop: FC = ({tokenName, reload, setReload, network, setNetwork}) => {
   const [provider, connection] = GetProvider(wallet, network);
   const publicKey = provider.wallet.publicKey;
   const [selectedOption, setSelectedOption] = useState("https://api.devnet.solana.com");
+  const [airdropPk, setAirdropPk] = useState("CONNECT WALLET");
   const [splAmount, setSplAmount] = useState(1000);
   const options = [
     { value: 'https://api.devnet.solana.com', label: 'DEVNET' },
     { value: 'https://api.testnet.solana.com', label: 'TESTNET' },
     { value: 'http://127.0.0.1:8899', label: 'LOCALNET' }
   ]
+
+  useEffect(() => {
+    if(wallet.publicKey) {
+        setAirdropPk(wallet.publicKey.toString()); 
+    }
+  }, [wallet.publicKey]);
 
   const handleChange = (option) => {
     setSelectedOption(option);
@@ -36,6 +43,14 @@ const AirDrop: FC = ({tokenName, reload, setReload, network, setNetwork}) => {
     setSplAmount(event.target.value);
   }
 
+  const handleChangeAirdropPk = (event) => {
+    setAirdropPk(event.target.value);
+  };
+
+  const handleFocusAirdropPk = (event) => {
+    setAirdropPk("");
+  }
+
   const handleSubmitSpl = async (event) => {
     event.preventDefault();
     await airdropSplTokens(splAmount, dummyMintPk, dummyMintPkBump);
@@ -44,8 +59,8 @@ const AirDrop: FC = ({tokenName, reload, setReload, network, setNetwork}) => {
   const handleSubmitSol = async (event) => {
     const [provider, connection] = GetProvider(wallet, network);
     let signature = '';
-    try {
-      signature = await connection.requestAirdrop(provider.wallet.publicKey, LAMPORTS_PER_SOL);
+    try { 
+      signature = await connection.requestAirdrop(new PublicKey(airdropPk), LAMPORTS_PER_SOL);
       notify('info', 'SOL airdrop requested:', signature, network);
       await connection.confirmTransaction(signature, 'processed');
       notify('success', 'SOL airdrop successful!', signature, network);
@@ -60,13 +75,15 @@ const AirDrop: FC = ({tokenName, reload, setReload, network, setNetwork}) => {
     try {
       const [provider, connection] = GetProvider(wallet, network);
       const program = new Program(idl, programID, provider);
+      const receiver = new PublicKey(airdropPk);
       let amountToAirdrop = new BN(amount * 1000000);
 
       let associatedTokenAccount = await Token.getAssociatedTokenAddress(
         ASSOCIATED_TOKEN_PROGRAM_ID,
         TOKEN_PROGRAM_ID,
         mintPda,
-        provider.wallet.publicKey,
+        receiver,
+        true
       );
 
       signature = await program.rpc.airdrop(
@@ -74,13 +91,14 @@ const AirDrop: FC = ({tokenName, reload, setReload, network, setNetwork}) => {
         amountToAirdrop,
         {
           accounts: {
-            payer: provider.wallet.publicKey,
             mint: mintPda,
             destination: associatedTokenAccount,
-            rent: web3.SYSVAR_RENT_PUBKEY,
-            tokenProgram: TOKEN_PROGRAM_ID,
+            payer: provider.wallet.publicKey,
+            receiver: receiver, 
             systemProgram: SystemProgram.programId,
-            associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID
+            tokenProgram: TOKEN_PROGRAM_ID,
+            associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+            rent: web3.SYSVAR_RENT_PUBKEY
           },
           signers: [],
         }
@@ -97,7 +115,7 @@ const AirDrop: FC = ({tokenName, reload, setReload, network, setNetwork}) => {
   return (
     <div className="airdrop-container">
       <div className="airdrop-wrapper">
-        <h3>NETWORK SELECTION</h3>
+        <h3>Network selection</h3>
         <div className="network-dropdown-wrapper">
           <Select
             className="network-dropdown navbar-button react-select-container"
@@ -119,7 +137,18 @@ const AirDrop: FC = ({tokenName, reload, setReload, network, setNetwork}) => {
             })}
            />
          </div>
-        <h3>SOL AIRDROP</h3>
+        <h3>Address for airdrop</h3>
+        <p>The address the SOL and {tokenName} will be sent to.</p>
+        <input
+          name="airdropPk"
+          type="text"
+          value={airdropPk}
+          onChange={handleChangeAirdropPk}
+          onFocus={handleFocusAirdropPk}
+          disabled={!publicKey}
+          className="airdrop-pk-input stake-input borrower-pk credix-button MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary balance-button"
+        />
+        <h3>SOL airdrop</h3>
         <p>Receive 1 SOL; you will need this to pay for transaction fees.</p>
         <Button
             variant="contained"
@@ -129,7 +158,7 @@ const AirDrop: FC = ({tokenName, reload, setReload, network, setNetwork}) => {
         >
          GET 1 SOL
         </Button>
-        <h3>{tokenName} AIRDROP</h3>
+        <h3>{tokenName} airdrop</h3>
         <p>Receive dummy SPL tokens, always coming from the same mint.</p>
         <p>Mint PK: Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr</p>
          <form className="form-row">
@@ -138,6 +167,7 @@ const AirDrop: FC = ({tokenName, reload, setReload, network, setNetwork}) => {
              defaultValue={1000}
              type="number"
              step="100"
+             disabled={!publicKey}
              className="navbar-button stake-input credix-button MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary balance-button"
             />
             <Button
@@ -149,9 +179,8 @@ const AirDrop: FC = ({tokenName, reload, setReload, network, setNetwork}) => {
              GET {tokenName}
             </Button>
          </form>
-         <h3>CUSTOMIZE THIS FAUCET</h3>
-         <p>Using the query parameter token-name, you can customize the UI to your needs. E.g. if we send the faucet to our test-users, we use the url <a href="https://spl-token-faucet.com?token-name=USDC" target="_blank">spl-token-faucet.com?token-name=USDC</a> to make the app look like a USDC faucet. If you want to mock the issuance of a TST token,
-         you could send the url <a href="https://spl-token-faucet.com?token-name=TST" target="_blank">spl-token-faucet.com?token-name=TST</a></p>
+         <h3>Customize this faucet</h3>
+         <p>Give DUMMY a different name: <a href="https://spl-token-faucet.com?token-name=USDC" target="_blank">spl-token-faucet.com?token-name=USDC</a></p>
        </div>
     </div>
   );
--- tests/spl-token-faucet.ts
+++ tests/spl-token-faucet.ts
@@ -2,6 +2,7 @@ import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
 const assert = require("assert");
 const { SystemProgram } = anchor.web3;
+import { PublicKey } from '@solana/web3.js';
 const { TOKEN_PROGRAM_ID, Token, ASSOCIATED_TOKEN_PROGRAM_ID } = require("@solana/spl-token");
 import { SplTokenFaucet } from '../target/types/spl_token_faucet';
 
@@ -10,9 +11,8 @@ describe('spl-token-faucet', () => {
   anchor.setProvider(anchor.Provider.env());
   const program = anchor.workspace.SplTokenFaucet as Program<SplTokenFaucet>;
 
-  async function airdropTokens(amount, mintPda, mintPdaBump, associatedTokenAccount) {
+  async function airdropTokens(amount, mintPda, mintPdaBump, receiver, associatedTokenAccount) {
     let amountToAirdrop = new anchor.BN(amount * 1000000);
-
     await program.rpc.airdrop(
       mintPdaBump,
       amountToAirdrop,
@@ -21,6 +21,7 @@ describe('spl-token-faucet', () => {
           payer: program.provider.wallet.publicKey,
           mint: mintPda,
           destination: associatedTokenAccount,
+          receiver: receiver, 
           rent: anchor.web3.SYSVAR_RENT_PUBKEY,
           tokenProgram: TOKEN_PROGRAM_ID,
           systemProgram: SystemProgram.programId,
@@ -31,8 +32,8 @@ describe('spl-token-faucet', () => {
     );
   }
 
-  async function getBalance(mintPda) {
-     const parsedTokenAccountsByOwner = await program.provider.connection.getParsedTokenAccountsByOwner(program.provider.wallet.publicKey, { mint: mintPda });
+  async function getBalance(receiver, mintPda) {
+     const parsedTokenAccountsByOwner = await program.provider.connection.getParsedTokenAccountsByOwner(receiver, { mint: mintPda });
      let balance = parsedTokenAccountsByOwner.value[0].account.data.parsed.info.tokenAmount.uiAmount;
 
      return balance;
@@ -44,24 +45,25 @@ describe('spl-token-faucet', () => {
       [Buffer.from(anchor.utils.bytes.utf8.encode("faucet-mint"))],
       program.programId);
 
-    console.log(mintPdaBump)
+    const receiver = new PublicKey("8hpvAu6cq6qzVM4NpXp9bH2uuT4PEYMJvrXKrSd5tdfR"); 
 
     let associatedTokenAccount = await Token.getAssociatedTokenAddress(
-      ASSOCIATED_TOKEN_PROGRAM_ID,
-      TOKEN_PROGRAM_ID,
-      mintPda,
-      program.provider.wallet.publicKey,
-    );
+        ASSOCIATED_TOKEN_PROGRAM_ID,
+        TOKEN_PROGRAM_ID,
+        mintPda,
+        receiver,
+        true
+      );
     // FIRST AIRDROP
     const firstAirdropAmount = 100;
-    await airdropTokens(firstAirdropAmount, mintPda, mintPdaBump, associatedTokenAccount);
-    let balance = await getBalance(mintPda);
+    await airdropTokens(firstAirdropAmount, mintPda, mintPdaBump, receiver, associatedTokenAccount);
+    let balance = await getBalance(receiver, mintPda);
     assert.ok(balance == firstAirdropAmount);
 
     // SECOND AIRDROP
     const secondAirdropAmount = 200;
-    await airdropTokens(secondAirdropAmount, mintPda, mintPdaBump, associatedTokenAccount);
-    balance = await getBalance(mintPda);
+    await airdropTokens(secondAirdropAmount, mintPda, mintPdaBump, receiver, associatedTokenAccount);
+    balance = await getBalance(receiver, mintPda);
     assert.ok(balance == firstAirdropAmount + secondAirdropAmount);
 
   });
